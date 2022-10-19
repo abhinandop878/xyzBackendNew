7f139dd8e8f1cf9d3ced521d6eb490cf6d7a6c7e
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-19 10:28:25 +0530

    XYZ backend app new

---
+++ src/main/java/com/nestDigital/xyzBackEndNew/XyzBackEndNewApplication.java
@@ -0,0 +1,13 @@
+package com.nestDigital.xyzBackEndNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBackEndNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzBackEndNewApplication.class, args);
+	}
+
+}
